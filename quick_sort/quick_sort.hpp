c4d342109843981da8f4c02f56df835383e772a1
Lai-YT <381xvmvbib@gmail.com>
2022-10-22 15:24:10 +0800

    quicksort: update comment

--- quick_sort/quick_sort.hpp
+++ quick_sort/quick_sort.hpp
@@ -2,27 +2,30 @@
 #define QUICK_SORT_HPP_
 
 #include <iostream>
-#include <vector>
 #include <utility>
+#include <vector>
+
+using Index = int;
 
 template<typename T>
-int Partition(std::vector<T>& arr, const int front, const int rear) {
-
+Index Partition(std::vector<T>& arr, const Index front, const Index rear) {
   const T pivot = arr.at(rear - 1);
-  int sp = front;
-  for (size_t i = front; i < rear; ++i) {
+  Index sp = front;
+  for (Index i = front; i < rear; ++i) {
     if (arr.at(i) <= pivot) {
       std::swap(arr.at(sp++), arr.at(i));
     }
   }
+  // the pivot is swapped into where it should be in the last iteration
+  // since pivot (rear - 1) == pivot
   return sp - 1;
 }
 
 // rear is excluded
 template<typename T>
-void QuickSort(std::vector<T>& arr, const int front, const int rear) {
+void QuickSort(std::vector<T>& arr, const Index front, const Index rear) {
   if (rear <= front) return;
-  int mid = Partition(arr, front, rear);
+  const Index mid = Partition(arr, front, rear);
   // mid is the true middle of sub-arr, no more move
   QuickSort(arr, front, mid);
   QuickSort(arr, mid + 1, rear);
